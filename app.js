50bccd0f923ef694795fc112b49fac39c9b1a45b
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2021-07-31 14:21:32 -0400

    initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('input');
+const form = document.querySelector('[data-js="card-weather"]')
+const city = document.querySelector('[data-js="city-name"]')
+const weather = document.querySelector('[data-js="city-weather"]')
+const temperature = document.querySelector('[data-js="city-temperature"]')
+const weatherIcon = document.querySelector('[data-js="weather-icon"]')
+const ilustrator = document.querySelector('[data-js="weather-img"]')
+const card = document.querySelector('[data-js="card"]')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log(localStorage.getItem('cityName'));
+  const inputValue = e.target.search.value.trim()
+  if(inputValue){
+    GetAndSetWeather(inputValue)
+  }
+  form.reset()
+})
+
+const GetAndSetWeather = async (inputValue)=> {
+  const [{Key, LocalizedName}] = await getCityData(inputValue)
+  const [{IsDayTime, WeatherIcon, WeatherText, Temperature}] = await getCityWeather(Key);
+  localStorage.setItem('cityName', LocalizedName)
+
+  city.textContent = LocalizedName
+  weather.textContent = WeatherText
+  temperature.textContent = Temperature.Metric.Value
+  weatherIcon.setAttribute('src', `./src/icons/${WeatherIcon}.svg`)
+  IsDayTime ? ilustrator.setAttribute('src', `./src/day.png`) : ilustrator.setAttribute('src', `./src/night.png`)
+  card.classList.remove('hidden')
+}
+
+GetAndSetWeather(localStorage.getItem('cityName'))
---
+++ weather.js
@@ -0,0 +1,21 @@
+const APIKey = "tJ5wkPUvxGt8X1uOK8KvWCwc7LhXVsHK"
+const baseUrl = 'http://dataservice.accuweather.com/'
+const getCityUrl = (cityName) => `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}&language=pt-br`
+const getWeatherUrl = (cityKey) => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br` 
+
+
+const fetchdata = async (url) => {
+  try {
+    const response = await fetch(url)
+    
+    if(!response.ok) {
+      throw new Error('Não foi possivel obter os dados')
+    }
+    return response.json()
+  } catch (error) {
+    alert(`${name}:${message}`)
+  }
+}
+
+const getCityData = cityName => fetchdata(getCityUrl(cityName))
+const getCityWeather = cityWeather => fetchdata(getWeatherUrl(cityWeather))
